9a4efbf4cd4e291d31bcd34504b3e62d2b5bf24f
Savio Moraes <smmsavio@hotmail.com>
2024-07-13 21:54:27 -0300

    feat: page folder structure

---
+++ frontend/src/pages/cart/cart.jsx
@@ -0,0 +1,8 @@
+export default function Cart() {
+  return (
+    <div>
+      <h1>Cart</h1>
+      <p>This is the Cart page.</p>
+    </div>
+  );
+}
---
+++ frontend/src/pages/plates/plate.jsx
@@ -0,0 +1,8 @@
+export default function Plate() {
+  return (
+    <div>
+      <h1>Plate</h1>
+      <p>This is the Plate page.</p>
+    </div>
+  );
+}
