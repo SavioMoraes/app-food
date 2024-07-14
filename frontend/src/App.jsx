689c5edaf9166ff6cc73d1d35e170e013ea7d650
Savio Moraes <smmsavio@hotmail.com>
2024-07-13 23:23:20 -0300

    feat: routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/navbar/navbar.jsx";
+import { Outlet } from "react-router-dom";
 
 export default function App() {
 
   return (
     <>
       <Navbar />
+      <Outlet />
     </>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/home/page.jsx';
+import Auth from './pages/auth/page.jsx';
+import Cart from './pages/cart/page.jsx';
+import Plate from './pages/plates/page.jsx';
+import Profile from './pages/profile/page.jsx';
+
+const pages = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/auth', element: <Auth /> },
+      { path: '/cart', element: <Cart /> },
+      { path: '/plates', element: <Plate /> },
+      { path: '/profile', element: <Profile /> },
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={pages}></RouterProvider>
   </React.StrictMode>,
 )
