3e72588f0e993e976e798384aa787291d9e04488
Savio Moraes <smmsavio@hotmail.com>
2024-07-15 17:40:34 -0300

    chore: refactore page auth

--- frontend/src/pages/auth/page.jsx
+++ frontend/src/pages/auth/page.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { TextField, Button } from '@mui/material';
 import styles from './page.module.css';
 import authServices from '../../services/auth.jsx';
+import { LuLogIn } from 'react-icons/lu';
 
 export default function Auth() {
   const [formType, setFormType] = useState('login');
@@ -40,74 +41,73 @@ export default function Auth() {
     }
   }
 
-
-  if (formType === 'login') {
-    return (
-      <div className={styles.authPageContainer}>
-        <h1>Login</h1>
-        <button onClick={handleChangeFormType}>Don't you hav an account? Click here</button>
-        <form onSubmit={handleSubmitForm}>
-          <TextField
-            required
-            label="Email"
-            type="email"
-            name="email"
-            onChange={handleFormDataChange}
-          />
-          <TextField
-            required
-            label="Password"
-            type="password"
-            name="password"
-            onChange={handleFormDataChange}
-          />
-          <Button type="submit">Login</Button>
-        </form>
-      </div>
-    );
+  if(authLoading) {
+    return ( <h1>Loading...</h1> )
   }
 
-  if (authLoading) {
-    return <h1>Loading...</h1>;
-  }
+return (
+    <div className={styles.authPageContainer}>
+        {formType === 'login' ? (
+            <>
+                <h1>Login</h1>
+                <button onClick={handleChangeFormType}>Don't you have an account? Click here</button>
+                <form onSubmit={handleSubmitForm}>
+                    <TextField 
+                    required
+                    label="Email"
+                    type="email"
+                    name="email"
+                    onChange={handleFormDataChange}
+                    />
+                    <TextField 
+                    required
+                    label="Password"
+                    type="password"
+                    name="password"
+                    onChange={handleFormDataChange}
+                    />
+                    <button type="submit">Login<LuLogIn /></button>
+                </form>
+            </>
+        ) : null}
 
-  if (formType === 'signup') {
-    return (
-      <div className={styles.authPageContainer}>
-        <h1>Signup</h1>
-        <button onClick={handleChangeFormType}>Already have an account? Click here</button>
-        <form onSubmit={handleSubmitForm}>
-          <TextField
-            required
-            label="Fullname"
-            type="fullname"
-            name="fullname"
-            onChange={handleFormDataChange}
-          />
-          <TextField
-            required
-            label="Email"
-            type="email"
-            name="email"
-            onChange={handleFormDataChange}
-          />
-          <TextField
-            required
-            label="Password"
-            type="password"
-            name="password"
-            onChange={handleFormDataChange}
-          />
-          <TextField
-            required
-            label="Confirm Password"
-            type="password"
-            name="confirmPassword"
-            onChange={handleFormDataChange}
-          />
-          <Button type="submit">Signup</Button>
-        </form>
-      </div>
-    );
-  }
+        {formType === 'signup' ? (
+            <>
+                <h1>Signup</h1>
+                <button onClick={handleChangeFormType}>Already have an account? Click here</button>
+                <form onSubmit={handleSubmitForm}>
+                    <TextField 
+                    required
+                    label="Fullname"
+                    type="fullname"
+                    name="fullname"
+                    onChange={handleFormDataChange}
+                    />
+                    <TextField 
+                    required
+                    label="Email"
+                    type="email"
+                    name="email"
+                    onChange={handleFormDataChange}
+                    />
+                    <TextField 
+                    required
+                    label="Password"
+                    type="password"
+                    name="password"
+                    onChange={handleFormDataChange}
+                    />
+                    <TextField 
+                    required
+                    label="Confirm password"
+                    type="password"
+                    name="confirmPassword"
+                    onChange={handleFormDataChange}
+                    />
+                    <button type="submit">Signup<LuLogIn /></button>
+                </form>
+            </>
+        ) : null}
+    </div>
+  );
 }
