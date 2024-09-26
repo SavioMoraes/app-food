df8466c02340a9847456b0a1d5aefb7e326ff845
Savio Moraes <smmsavio@hotmail.com>
2024-09-26 16:24:39 -0300

    chore: consuming api post method

--- frontend/src/contexts/useCartContext.jsx
+++ frontend/src/contexts/useCartContext.jsx
@@ -1,50 +1,56 @@
+
 import { createContext, useContext, useState } from "react";
 
 const CartContext = createContext();
 
 export function CartProvider({ children }) {
-  const [cartItems, setCartItems] = useState([]);
+    const [cartItems, setCartItems] = useState([]);
 
-  const addToCart = (itemToAdd) => {
-    const checkItemAlready = cartItems.find((cartItem) => {
-      return cartItem._id === itemToAdd._id
-    });
+    const addToCart = (itemToAdd) => {
+        const checkItemAlready = cartItems.find((cartItem) => {
+            return cartItem._id === itemToAdd._id;
+        })
 
-    if (!checkItemAlready) {
-      itemToAdd.quantity = 1;
+        if(!checkItemAlready) {
+            itemToAdd.quantity = 1;
 
-      setCartItems([...cartItems, itemToAdd]);
-      console.log('Item added correctly');
-      
-    } else {
-      console.log('Item already on cart');
-    }    
-  }
+            setCartItems([...cartItems, itemToAdd]);
+            console.log('Item added correctly');
+        } else {
+            console.log('Item is already on cart');  
+        }
+    }
 
-  const removeFromCart = (itemId) => {
-    const cartItemsSanitized = cartItems.filter((item) => {
-      return item._id !== itemId;
-    });
-    setCartItems(cartItemsSanitized);
-  }
-  
-  const updateCartItems = (items) => {
-    setCartItems(items);
-  }
+    const removeFromCart = (itemId) => {
+        const cartItemsSanitized = cartItems.filter((item) => {
+            return item._id !== itemId;
+        })
+        
+        setCartItems(cartItemsSanitized);
 
-  return (
-    <CartContext.Provider value={{ removeFromCart, addToCart, cartItems, updateCartItems }}>
-      {children}
-    </CartContext.Provider>
-  );
+    }
+
+    const updateCartItems = (items) => {
+        setCartItems(items);
+    }
+
+    const clearCart = () => {
+        setCartItems([]);
+    }
+
+    return(
+        <CartContext.Provider value={{ removeFromCart, addToCart, cartItems, updateCartItems, clearCart }}>
+            {children}
+        </CartContext.Provider>
+    )
 }
 
 export const useCartContext = () => {
-  const context = useContext(CartContext);
+    const context = useContext(CartContext);
 
-  if (!context) {
-    console.log("you are out of CartContext");
-  }
+    if(!context) {
+        console.log('you are out of CartContext');
+    }
 
-  return context;
+    return context;
 }
--- frontend/src/pages/cart/page.jsx
+++ frontend/src/pages/cart/page.jsx
@@ -1,10 +1,15 @@
 import { useCartContext } from "../../contexts/useCartContext";
 import styles from "./page.module.css";
 import { LuMinusCircle } from "react-icons/lu";
+import { useState } from "react";
+import ConfirmOrderPopup from "../../components/confirmOrderPopup/confirmOrderPopup.jsx";
+import orderServices from "../../services/order.jsx";
 
 export default function Cart() {
   
-  const { cartItems, updateCartItems, removeFromCart } = useCartContext();
+  const { cartItems, updateCartItems, removeFromCart, clearCart } = useCartContext();
+  const [confirmPopupOpen, setConfirmPopupOpen] = useState(false);
+  const { sendOrder } = orderServices();
 
   const handleChangeItemQty = (mode, itemId) => {    
     const updatedCartItem = cartItems.map((item) => {
@@ -21,6 +26,23 @@ export default function Cart() {
     updateCartItems(updatedCartItem);
   }
 
+  const handleOpenPopup = (e) => {
+    e.preventDefault();
+    setConfirmPopupOpen(!confirmPopupOpen);
+  }
+
+  const handleConfirmOrder = (orderData) => {
+    orderData.items = cartItems.map((item) => {
+      return {
+        plateId: item._id,
+        quantity: item.quantity
+      }
+    });
+    sendOrder(orderData);
+    setConfirmPopupOpen(!confirmPopupOpen);
+    clearCart();
+  }
+
   if (!cartItems.length) {
     return (
       <div>
@@ -31,34 +53,38 @@ export default function Cart() {
   }
   
   return (
-    <div className={styles.pageContainer}>
-      <h1>Your items:</h1>
-      <section>
-        <div className={styles.itemsListContainer}>
-          {cartItems.map((item) => (
-            <div key={item._id} className={styles.itemContainer}>
-              <img src={item.imgUrl} alt="" />
-              <div className={styles.itemContent}>
-                <h2>{item.name}</h2>
-                <p>{item.price}</p>
-                <p>{String(item.ingrdients)}</p>
-                <p>{item.description}</p>
-                <div className={styles.portionContainer}>
-                  <p>Portions:</p>
-                  <p>{item.quantity}</p>
-                  <div className={styles.portionBtns}>
-                    <button onClick={() => {handleChangeItemQty('less', item._id)}}>-</button>
-                    <button onClick={() => {handleChangeItemQty('more', item._id)}}>+</button>
+    <>
+    
+      <div className={styles.pageContainer}>
+        <h1>Your items:</h1>
+        <section>
+          <div className={styles.itemsListContainer}>
+            {cartItems.map((item) => (
+              <div key={item._id} className={styles.itemContainer}>
+                <img src={item.imgUrl} alt="" />
+                <div className={styles.itemContent}>
+                  <h2>{item.name}</h2>
+                  <p>{item.price}</p>
+                  <p>{String(item.ingrdients)}</p>
+                  <p>{item.description}</p>
+                  <div className={styles.portionContainer}>
+                    <p>Portions:</p>
+                    <p>{item.quantity}</p>
+                    <div className={styles.portionBtns}>
+                      <button onClick={() => {handleChangeItemQty('less', item._id)}}>-</button>
+                      <button onClick={() => {handleChangeItemQty('more', item._id)}}>+</button>
+                    </div>
                   </div>
+                  <button onClick={() => { removeFromCart(item._id) }}><LuMinusCircle /> Remove item</button>
                 </div>
-                <button onClick={() => { removeFromCart(item._id) }}><LuMinusCircle /> Remove item</button>
               </div>
-            </div>
-          ))}
-        </div>
-      </section>
+            ))}
+          </div>
+        </section>
+        <button className={styles.confirmBtn} onClick={handleOpenPopup}>Confirm your order!</button>
+      </div>
 
-      <button className={styles.confirmBtb}>Confirm your order!</button>
-    </div>
+      <ConfirmOrderPopup open={confirmPopupOpen} onClose={handleOpenPopup} onConfirm={handleConfirmOrder} />
+    </>
   );
 }
